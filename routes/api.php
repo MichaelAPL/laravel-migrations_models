451e6951a88fa89eeed7e662e347aa50758795c7
Miguel Pat <michaelapl96@gmail.com>
2017-02-15 20:23:08 -0600

    Adding routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*Salesmen routes*/
+Route::get('salesmen', 'SalesmenController@index');
+Route::get('salesmen/{id}', 'SalesmenController@show');
+Route::post('salesmen', 'SalesmenController@store');
+Route::put('salesmen/{id}', 'SalesmenController@update');
+Route::patch('salesmen/{id}', 'SalesmenController@patch');
+Route::delete('salesmen/{id}', 'SalesmenController@destroy');
+Route::post('salesmen/directions/{id}', 'SalesmenController@store');
+Route::put('salesmen/directions/{id}', 'SalesmenController@update');
